bea70e8c901f4c9b28812697bb439d980c0f3fd2
Henry WL <henrywl@gmail.com>
2021-02-10 20:04:53 +0000

    Have basic functions working and displaying library to page

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const LibDivInner = document.getElementById('LibInner')
+
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
@@ -10,3 +14,38 @@ function Book(title, author, pages, read) {
 
 const EastofEden = new Book('East of Eden', 'John Steinbeck', 732, "yes")
 console.log(EastofEden.info())
+
+const Lotr = new Book('Lord of the rings', 'Toklkien', 837, "no")
+console.log(Lotr.info())
+
+function addBookToLibrary(book) {
+    book = EastofEden
+    book1 = Lotr
+    myLibrary.push(book)
+    myLibrary.push(book1)
+
+}
+
+function addBookTest() {
+    title = prompt('Enter a title')
+    author = prompt('Enter author')
+    pages = prompt('Enter page count')
+    readyesno = prompt('Have you read this book?')
+
+    const newBook = new Book(title, author, pages, readyesno)
+    console.log(newBook)
+    myLibrary.push(newBook)
+
+}
+
+addBookTest()
+
+function LibraryLoop() {
+    for (let i = 0; i < myLibrary.length; i++){
+        LibDivInner.innerHTML += myLibrary[i].info() + ' + '
+    }
+}
+
+addBookToLibrary()
+LibraryLoop()
+
